b72c985470ff52bf3a23611226eb488cdf0f602f
Nimisha Jha <nimishajha1103@gmail.com>
2023-07-30 17:05:46 +0530

    commit

---
+++ google/google.js
@@ -0,0 +1,5 @@
+console.log("scrooptnjtr")
+console.log(document.getElementsByTagName('img'))
+// console.log(document.getElementsByClassName('appdiv'))
+
+console.log(document.querySelectorAll(''))
